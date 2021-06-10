5026ce1aa03e5a8691849cf16d5203e138c20b06
AsfariaChowdhury <72992635+AsfariaChowdhury@users.noreply.github.com>
2021-06-11 00:30:56 +0600

    Created more tables/edited
    1. branch_id in employee table is a foreign key, refers to branch_id in branch table.
    
    2. manager_id in employee table refers to emp_id in employee table.
    
    3. client table is created. branch_id in client table is a foreign key, and refers to branch_id in branch table.
    
    4. product table is created. The two primary keys are branch_id and client_id. branch_id in product table is a foreign key that refers to branch_id in client table. client_id in product table is a foreign key that refers to client_id in client table.

--- CompanyDatabase.sql
+++ CompanyDatabase.sql
@@ -15,3 +15,34 @@ CREATE TABLE branch(
     mgr_start_date DATE,
     FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
 );
+
+-- 11/06/2021
+ALTER TABLE employee
+ADD FOREIGN KEY(branch_id)
+REFERENCES branch(branch_id)
+ON DELETE SET NULL;
+
+ALTER TABLE employee
+ADD FOREIGN KEY(manager_id)
+REFERENCES employee(emp_id)
+ON DELETE SET NULL;
+
+CREATE TABLE client(
+	client_id INT PRIMARY KEY,
+    client_name VARCHAR(40),
+    branch_id INT,
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL
+);
+
+CREATE TABLE product(
+	branch_id INT,
+    client_id INT,
+    quantity INT,
+    product_name VARCHAR(80),
+    PRIMARY KEY(branch_id, client_id),
+    FOREIGN KEY(branch_id) REFERENCES client(branch_id) ON DELETE CASCADE,
+    FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
+
+-- DROP TABLE product;
+
